fd2581aeb0a1c8a633f42ee59fd27e7e5ecd963c
hnk <217316986+hnkcodes@users.noreply.github.com>
2026-09-21 10:55:52 +0900

    Fix initial IP location detection

--- api/location.js
+++ api/location.js
@@ -1,14 +1,16 @@
 export default async function handler(req, res) {
-  const apiKey = process.env.IPIFY_API_KEY;
   const { ipAddress, domain } = req.query;
+  const clientIp = req.headers["x-forwarded-for"];
   const params = new URLSearchParams({
     apiKey,
   });
+
   if (ipAddress) {
     params.append("ipAddress", ipAddress);
-  }
-  if (domain) {
+  } else if (domain) {
     params.append("domain", domain);
+  } else if (clientIp) {
+    params.append("ipAddress", clientIp);
   }
 
   const response = await fetch(
